65068aa2f0528bac6ab98d58f3a68bbecb682942
Liam Stewart <liamchristopher.media@gmail.com>
2021-05-26 10:16:36 -0700

    leaving router blank

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -9,7 +9,7 @@ import About from './Pages/About';
 function App() {
   return (
     <div>
-      <Router basename={process.env.PUBLIC_URL}>
+      <Router>
         <Navigation />
         <Route exact path='/' component={Home}/>
         <Route exact path='/Work' component={Work}/>
