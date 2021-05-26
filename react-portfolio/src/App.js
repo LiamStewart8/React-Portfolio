9267b0f9986c3217cfd218daab8ec8bbdceb3405
Liam Stewart <liamchristopher.media@gmail.com>
2021-05-25 23:53:44 -0700

    changed to Hashrouter

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import Navigation from './Components/Navbar';
 import Footer from './Components/Footer';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { HashRouter as Router, Route } from "react-router-dom";
 import Home from './Pages/Home';
 import Work from './Pages/Work';
 import About from './Pages/About';
